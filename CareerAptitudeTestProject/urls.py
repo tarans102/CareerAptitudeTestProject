84c6acd176411cfcee820ff24f680864dacc0b50
tarans102 <tarans102@gmail.com>
2024-07-30 23:38:29 +0530

    Changed supported languages and added redirect

--- CareerAptitudeTestProject/urls.py
+++ CareerAptitudeTestProject/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from CATApp import urls
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/CATApp/", permanent=False)),
     path("admin/", admin.site.urls),
     path("CATApp/", include("CATApp.urls")),
 ]
